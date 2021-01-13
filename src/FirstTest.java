caa92ea40156c9be918e7a0b124098556b94280b
Elvira Mak <efilia@bk.ru>
2021-01-13 17:47:05 +0300

    Написан тест проверяющий текст элемента

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -2,11 +2,15 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -35,6 +39,37 @@ public class FirstTest {
 
    @Test
     public void firstTest(){
-        System.out.println("Java+Appium=start test!!!");
+       System.out.println("Java+Appium=start test!!!");
+       waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+       String searchText = waitElementPresentBy(By.xpath("//*[contains(@text,'Search Wikipedia')]")).getText();
+       System.out.println(searchText);
+       //waitElementPresentBy(By.xpath("//android.widget.HorizontalScrollView[@content-desc=\"Page 1 of 4\"]/android.widget.LinearLayout/android.widget.LinearLayout[4]")).click();
+       //waitElementPresentBy(By.id("org.wikipedia.beta:id/bottomOffset")).click();
+      System.out.println("Java+Appium=finish test!!!");
+    }
+    @Test
+    public void elementsTextAssertionTest(){
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+       Boolean result =  assertElementHasText(By.xpath("//*[contains(@text,'Search Wikipedia')]"), "Search Wikipedia",
+                "Elements text is not = Search Wikipedia");
+        System.out.println("result= "+result);
+       Assert.assertTrue(result);
+    }
+
+    public WebElement waitElementPresentBy(By locator){
+        WebDriverWait webDriverWait = new WebDriverWait(driver,5);
+        webDriverWait.withMessage("No element present by locator: "+ locator);
+        return webDriverWait.until(ExpectedConditions.presenceOfElementLocated(locator));
+
+    }
+    public boolean assertElementHasText(By locator, String expectedText,String errorMessage){
+        String gettingText = waitElementPresentBy(locator).getAttribute("name");
+        System.out.println("expected= "+expectedText);
+        System.out.println("getting= " + gettingText);
+        if (gettingText.equals(expectedText)){
+            return true;
+        }
+        System.out.println(errorMessage);;
+        return false;
     }
 }
