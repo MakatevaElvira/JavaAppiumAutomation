81954e2a5f1e2fbc696d684cac4fe7e6b0d05f8f
Elvira Mak <efilia@bk.ru>
2021-01-17 13:58:21 +0300

    Реализована отмена и проверка слов в поиске

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class FirstTest {
     private AppiumDriver driver;
@@ -55,11 +56,63 @@ public class FirstTest {
         System.out.println("result= "+result);
        Assert.assertTrue(result);
     }
+    @Test
+    public void searchCancellationTest(){
+        String toFind = "google";
+        //пропустить
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+        //кликнуть поиск
+        //waitElementPresentBy(By.xpath("//*[contains(@text,'Search Wikipedia')]")).click();//@class android.widget.TextView
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Search Wikipedia')]")).click();//org.wikipedia.beta:id/search_container
+        //(By.id("//org.wikipedia.beta:id/search_container")).click();//
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind);
+        // проверить в результате есть записи
+        waitElementsPresentBy(By.id("org.wikipedia.beta:id/page_list_item_title"));
+        // нажать крестик-очистить поиск
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_close_btn")).click();
+        // проверить результат = пустой поиск=тоже работает
+        //waitElementPresentBy(By.id("org.wikipedia.beta:id/search_empty_image"));
+        // либо нотпрезент
+        Assert.assertTrue(waitElementNotPresentBy(By.id("org.wikipedia.beta:id/page_list_item_title")));
+
+    }
+    @Test
+    public void searchCheckingResultTest(){
+        String toFind = "Google";
+        //пропустить
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+        //кликнуть поиск
+        //waitElementPresentBy(By.xpath("//*[contains(@text,'Search Wikipedia')]")).click();//@class android.widget.TextView
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Search Wikipedia')]")).click();//org.wikipedia.beta:id/search_container
+        //(By.id("//org.wikipedia.beta:id/search_container")).click();//
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind);
+        // проверить результата assert
+        List <WebElement> results = driver.findElements(By.id("org.wikipedia.beta:id/page_list_item_title"));
+        for ( WebElement result: results){
+            System.out.println("Result= "+ result.getText());
+            Assert.assertTrue(result.getText().contains(toFind));
+        }
+
+    }
 
     public WebElement waitElementPresentBy(By locator){
         WebDriverWait webDriverWait = new WebDriverWait(driver,5);
         webDriverWait.withMessage("No element present by locator: "+ locator);
         return webDriverWait.until(ExpectedConditions.presenceOfElementLocated(locator));
+
+    }
+    public List <WebElement> waitElementsPresentBy(By locator){
+        WebDriverWait webDriverWait = new WebDriverWait(driver,5);
+        webDriverWait.withMessage("No element present by locator: "+ locator);
+        return webDriverWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+
+    }
+    public Boolean waitElementNotPresentBy(By locator){
+        WebDriverWait webDriverWait = new WebDriverWait(driver,5);
+        webDriverWait.withMessage("No element present by locator: "+ locator);
+        return webDriverWait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
 
     }
     public boolean assertElementHasText(By locator, String expectedText,String errorMessage){
