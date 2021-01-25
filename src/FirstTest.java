422077f3309a05fe00f8e81d009cd0befa71a245
Elvira Mak <efilia@bk.ru>
2021-01-26 01:35:43 +0300

    Продвинутые тесты + добавление 2х статей

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,19 +1,24 @@
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.ActionOptions;
+
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.time.Duration;
 import java.util.List;
+
+import static java.time.Duration.ofMillis;
 
 public class FirstTest {
     private AppiumDriver driver;
@@ -94,13 +99,193 @@ public class FirstTest {
             System.out.println("Result= "+ result.getText());
             Assert.assertTrue(result.getText().contains(toFind));
         }
+    }
+    @Test
+    public void swipeTest() throws InterruptedException {
+        String toFind = "Google";
+        //пропустить
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+        //кликнуть поиск
+        //waitElementPresentBy(By.xpath("//*[contains(@text,'Search Wikipedia')]")).click();//@class android.widget.TextView
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Search Wikipedia')]")).click();//org.wikipedia.beta:id/search_container
+        //(By.id("//org.wikipedia.beta:id/search_container")).click();//
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind);
+        //открыть статью
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/page_list_item_title")).click();
+        //свайпнуть
+        swipeUp(2000);
+        swipeUp(2000);
+        swipeUp(2000);
+    }
+    @Test
+    public void swipeBeforeElementTest() throws InterruptedException {
+        String toFind = "Appium";
+        //пропустить
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+        //кликнуть поиск
+        //waitElementPresentBy(By.xpath("//*[contains(@text,'Search Wikipedia')]")).click();//@class android.widget.TextView
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Search Wikipedia')]")).click();//org.wikipedia.beta:id/search_container
+        //(By.id("//org.wikipedia.beta:id/search_container")).click();//
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind);
+        //открыть статью
+        waitElementPresentBy(By.xpath("//*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='Appium']")).click();
+        //свайпнуть
+        swipeUpToFindElement(By.xpath("//*[@text='View article in browser']"),10);
+    }
 
+    @Test
+    public void bigTest() throws InterruptedException {
+        String toFind = "Appium";
+        //пропустить
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+        //кликнуть поиск
+        //waitElementPresentBy(By.xpath("//*[contains(@text,'Search Wikipedia')]")).click();//@class android.widget.TextView
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Search Wikipedia')]")).click();//org.wikipedia.beta:id/search_container
+        //(By.id("//org.wikipedia.beta:id/search_container")).click();//
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind);
+        //открыть статью
+        waitElementPresentBy(By.xpath("//*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='Appium']")).click();
+        //нажать три точки =не надо org.wikipedia.beta:id/article_menu_bookmark
+        //waitElementPresentBy(By.xpath("//android.widget.ImageView[@content-desc=\"More options\"]")).click();
+        //нажать SAVE org.wikipedia.beta:id/article_menu_bookmark
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/article_menu_bookmark")).click();
+        //нажать ADD TO LIST
+        waitElementPresentBy(By.xpath("//android.widget.Button[contains(@text,'ADD TO LIST')]")).click();
+        //КЛИК чтобы закрылась всплывашка //класс android.widget.ListView
+        //waitElementPresentBy(By.xpath("//android.widget.ListView")).click();
+        //перейти на активную часть экрана org.wikipedia.beta:id/touch_outside
+         waitElementPresentBy(By.id("org.wikipedia.beta:id/touch_outside")).click();
+        //или перейти напрямую в сохраненные
+       // waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Saved')]")).click();
+        //открыть вью лист
+        //waitElementPresentBy(By.xpath("//android.widget.Button[contains(@text,'VIEW LIST')]")).click();
+
+        // выйти из статьи ?
+        waitElementPresentBy(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]")).click();
+        // выйти из поиска статей?
+        //waitElementPresentBy(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]")).click();
+        //закрыть поиск org.wikipedia.beta:id/search_close_btn
+        //обратно	android.widget.ImageButton
+        waitElementPresentBy(By.xpath("//android.widget.ImageButton")).click();
+
+        //Открыть мои статьи на гл экране
+        waitElementPresentBy(By.xpath("//android.widget.FrameLayout[@content-desc=\"My lists\"]")).click();
+        //Открыть Сохраненные
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Saved')]")).click();
+        //проверить наличие статьи id=
+        //By.xpath("//*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='Appium']")
+       // By articleLocation = By.xpath("//android.widget.TextView[@text='Appium']");
+        By articleLocation = By.xpath("//android.view.ViewGroup/*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='Appium']");
+        waitElementPresentBy(articleLocation);
+        //свайпнуть и удалить
+        swipeUpElementToLeft(articleLocation,200);
+        // проверить что статья удалилась, неотображается
+        Assert.assertTrue(waitElementNotPresentBy(articleLocation));
+    }
+    @Test
+    public void twoArticleTest() throws InterruptedException {
+        String toFind1 = "Appium";
+        String toFind2 = "Mandarin";
+        //пропустить
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+        //кликнуть поиск
+        //waitElementPresentBy(By.xpath("//*[contains(@text,'Search Wikipedia')]")).click();//@class android.widget.TextView
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Search Wikipedia')]")).click();//org.wikipedia.beta:id/search_container
+        //(By.id("//org.wikipedia.beta:id/search_container")).click();//
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind1);
+        //открыть статью
+        waitElementPresentBy(By.xpath("//*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='"+toFind1+"']")).click();
+        //нажать SAVE org.wikipedia.beta:id/article_menu_bookmark
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/article_menu_bookmark")).click();
+        //нажать ADD TO LIST
+        waitElementPresentBy(By.xpath("//android.widget.Button[contains(@text,'ADD TO LIST')]")).click();
+        //перейти на активную часть экрана org.wikipedia.beta:id/touch_outside
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/touch_outside")).click();
+        // выйти из статьи на Поиск?
+        waitElementPresentBy(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]")).click();
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).clear();
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind2);
+        //открыть статью
+        waitElementPresentBy(By.xpath("//*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='"+toFind2+"']")).click();
+        //нажать SAVE org.wikipedia.beta:id/article_menu_bookmark
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/article_menu_bookmark")).click();
+        //нажать ADD TO LIST
+        waitElementPresentBy(By.xpath("//android.widget.Button[contains(@text,'ADD TO LIST')]")).click();
+        //перейти на активную часть экрана org.wikipedia.beta:id/touch_outside
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/touch_outside")).click();
+        // выйти из статьи на Поиск?
+        waitElementPresentBy(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]")).click();
+        //обратно	из Поиска
+        waitElementPresentBy(By.xpath("//android.widget.ImageButton")).click();
+        //Открыть мои статьи на гл экране
+        waitElementPresentBy(By.xpath("//android.widget.FrameLayout[@content-desc=\"My lists\"]")).click();
+        //Открыть Сохраненные
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Saved')]")).click();
+        //проверить наличие статьи id=
+        By articleLocation1 = By.xpath("//android.view.ViewGroup/*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='"+toFind1+"']");
+        By articleLocation2 = By.xpath("//android.view.ViewGroup/*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='"+toFind2+"']");
+        waitElementPresentBy(articleLocation1);
+        waitElementPresentBy(articleLocation2);
+        //свайпнуть и удалить
+        swipeUpElementToLeft(articleLocation2,200);
+        // проверить что статья удалилась, неотображается
+        Assert.assertTrue(waitElementNotPresentBy(articleLocation2));
+        Assert.assertTrue(driver.findElement(articleLocation1).getText().equals(toFind1));
+        driver.findElement(By.id("org.wikipedia.beta:id/page_list_item_title")).click();
+        String afterSwipeTitle = driver.findElement(By.xpath("//android.view.View[@text='"+toFind1+"']")).getText();
+        Assert.assertEquals(toFind1,afterSwipeTitle);
+    }
+
+    @Test
+    public void rotationTest(){
+        String toFind = "How deep is your love";
+        //пропустить
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+        //кликнуть поиск
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Search Wikipedia')]")).click();
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind);
+        //открыть статью
+        //waitElementPresentBy(By.id("org.wikipedia.beta:id/page_list_item_title")).click();
+        //waitElementPresentBy(By.xpath("//*[@resource-id='org.wikipedia.beta:id/page_list_item_title']//*[contains(@text,'"+toFind+"')]")).click();
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/page_list_item_title")).click();
+        //проверить заголовок
+        By titleLocator = By.xpath("//android.view.View");
+        String beforeRotationTitle = findElementAndGetAttribute(titleLocator,"text");
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+        String afterRotationTitle = findElementAndGetAttribute(titleLocator,"text");
+        Assert.assertEquals(beforeRotationTitle,afterRotationTitle);
+    }
+    @Test
+    public void backGroundTest(){
+        String toFind = "mandarin";
+        //пропустить
+        waitElementPresentBy(By.xpath("//*[contains(@text,'SKIP')]")).click();
+        //кликнуть поиск
+        waitElementPresentBy(By.xpath("//android.widget.TextView[contains(@text,'Search Wikipedia')]")).click();
+        //в нов поле ввода ввести Значение
+        waitElementPresentBy(By.id("org.wikipedia.beta:id/search_src_text")).sendKeys(toFind);
+        //найти статью
+        waitElementPresentBy(By.xpath("//android.view.ViewGroup/*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='Mandarin Chinese']"));
+        driver.runAppInBackground(ofMillis(25));
+        waitElementPresentBy(By.xpath("//android.view.ViewGroup/*[@resource-id='org.wikipedia.beta:id/page_list_item_title'][@text='Mandarin Chinese']"));
     }
 
     public WebElement waitElementPresentBy(By locator){
         WebDriverWait webDriverWait = new WebDriverWait(driver,5);
         webDriverWait.withMessage("No element present by locator: "+ locator);
         return webDriverWait.until(ExpectedConditions.presenceOfElementLocated(locator));
+
+    }
+    public Boolean waitElementIsPresentBy(By locator){
+        WebDriverWait webDriverWait = new WebDriverWait(driver,10);
+        webDriverWait.withMessage("No element present by locator: "+ locator);
+        return webDriverWait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
 
     }
     public List <WebElement> waitElementsPresentBy(By locator){
@@ -124,5 +309,48 @@ public class FirstTest {
         }
         System.out.println(errorMessage);;
         return false;
+    }
+
+    public void swipeUp(int timeOfSwipe)  {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width/2;
+        int startY = (int)(size.width * 0.8);
+        int endY = (int)(size.width * 0.2);
+        PointOption startPoint = new PointOption().withCoordinates(x,startY);
+        PointOption endPoint = new PointOption().withCoordinates(x,endY);
+        action.press(startPoint).waitAction(new WaitOptions().withDuration(ofMillis(timeOfSwipe))).moveTo(endPoint).release().perform();
+    }
+    public void swipeQuick() {
+        swipeUp(200);
+    }
+    public void swipeUpToFindElement(By by, int maxSwipes) {
+        int alreadySwipe = 0;
+        while (driver.findElements(by).size() == 0) {
+            if (alreadySwipe > maxSwipes){
+                waitElementPresentBy(by);
+                return;
+            }
+            swipeQuick();
+            ++alreadySwipe;
+        }
+    }
+    public void swipeUpElementToLeft(By by, int timeOfSwipe) {
+        WebElement element = waitElementPresentBy(by);
+        int leftX = (int)(element.getLocation().getX()* 0.1);
+        int rightX = (int)( (leftX + element.getSize().getWidth()) * 0.9);//прибавили к левой точке ширину элемента и получили правую
+        int upperY = element.getLocation().getY();
+        int lowerY = upperY + element.getSize().getHeight();//прибавили к верхней точке высоту элемента и получили нижнюю
+        int middleY = (upperY + lowerY )/2;
+        PointOption startPoint = new PointOption().withCoordinates(rightX, middleY);
+        PointOption endPoint = new PointOption().withCoordinates(leftX,middleY);
+        TouchAction action = new TouchAction(driver);
+        action.press(startPoint).waitAction(new WaitOptions().withDuration(ofMillis(timeOfSwipe))).moveTo(endPoint).release().perform();
+
+    }
+
+    public String findElementAndGetAttribute( By locator, String attribute){
+        return waitElementPresentBy(locator).getAttribute(attribute);
+
     }
 }
